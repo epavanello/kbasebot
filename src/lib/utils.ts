52fedc1264b09999c4c147346397772170454929
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2023-08-30 18:32:16 +0200

    fix error management

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -83,3 +83,18 @@ export const getRandomArrayIndex = (arr) =>
 export const getRandomArrayValue = (arr = []) => {
   return arr[Math.floor(Math.random() * arr?.length)];
 };
+
+export function getErrorMessage(e: unknown): string {
+	try {
+		if (typeof e === 'string') {
+			return e;
+		} else if (e instanceof Error) {
+			return e.message;
+		} else if (typeof e == 'object' && e && 'data' in e && typeof e.data == 'string') {
+			return JSON.parse(e.data)?.message || '';
+		}
+	} catch (error) {
+		return getErrorMessage(error);
+	}
+	return 'unknown';
+}
