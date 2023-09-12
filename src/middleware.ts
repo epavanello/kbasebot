40b8bec956e6dee750b39b6cd41d38f1e5e39ae1
nilooy <nilooy.dev@gmail.com>
2023-09-12 12:30:34 +0200

    :fire: Add middleware for private route

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,9 +7,29 @@ import type { Database } from "@/lib/types/database.types";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
-  const session = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  // console.log({ session });
+  const url = req.nextUrl;
+  const mainPath = url.pathname.split("/").filter(Boolean)?.[0] || "";
+
+  if (!session) {
+    // handle internal public routes
+    if (internalPublicRoutes.includes(url.pathname)) {
+      return res;
+    } else if (includedMainPaths.includes(mainPath)) {
+      return NextResponse.redirect(new URL("/auth", req.url));
+    }
+  }
 
   return res;
 }
+
+const internalPublicRoutes = ["/api/chatbots/message"];
+
+const includedMainPaths = ["app", "api"];
+
+export const config = {
+  matcher: includedMainPaths.map((i) => `/${i}/:path*`),
+};
