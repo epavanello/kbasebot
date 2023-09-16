db3a83dc29e9a5d744601a46806bb7208addca1c
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2023-09-16 19:11:05 +0200

    fix

--- src/components/ui/payment-block.tsx
+++ src/components/ui/payment-block.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Icon } from "@/components/ui/icons";
@@ -18,16 +18,12 @@ const PaymentBlock = ({
         <div className="blur-content flex justify-center items-center flex-col gap-4">
           <Icon icon={"solar:lock-linear"} className="text-6xl opacity-70" />
           <h2 className="text-2xl font-semibold opacity-70">{text}</h2>
-          <Button
-            className=""
-            size="lg"
-            variant=""
-            onClick={() => {
-              redirect("/app/account/plan");
-            }}
+          <Link
+            className={cn(buttonVariants({ variant: "default", size: "lg" }))}
+            href="/pricing"
           >
             {btnText}
-          </Button>
+          </Link>
         </div>
       )}
     </div>
