aefc288bce151ad625f4a556bd99455aaa7a4913
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2023-09-17 08:43:24 +0200

    mail link

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -8,12 +8,19 @@ export function Footer() {
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-4 lg:px-8">
         <div className="border-t border-black border-opacity-10 dark:border-white dark:border-opacity-10 pt-8 flex flex-col md:flex-row items-center gap-4 md:justify-between">
           <div className="flex items-center gap-4 md:order-2">
-            <a
+          <a
               target="_blank"
               href="https://twitter.com/kbasebot"
               className="text-gray-500 hover:opacity-50"
             >
-              <Icon icon="mdi:twitter" className="w-7 h-7" />
+              <Icon icon="mdi:twitter" className="w-6 h-6" />
+            </a>
+            <a
+              target="_blank"
+              href="mailto:hello@kbasebot.com"
+              className="text-gray-500 hover:opacity-50"
+            >
+              <Icon icon="line-md:email-opened" className="w-6 h-6" />
             </a>
             {/* <a href="#" className="text-gray-500 hover:opacity-50">
               <Icon icon="mdi:github" className="w-7 h-7" />
