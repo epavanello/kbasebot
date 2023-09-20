c1882fa16f4776aa896a749a098cbafc6a649789
Emanuele Pavanello <pavanello.emanuele@gmail.com>
2023-09-20 22:42:27 +0200

    order chatbot

--- src/lib/hooks/use-chatbots.ts
+++ src/lib/hooks/use-chatbots.ts
@@ -22,6 +22,7 @@ export function useChatbots() {
                 .select()
                 .eq("user_id", user?.id!)
                 .eq("status", "READY")
+                .order("created_at", { ascending: true })
                 .throwOnError()
             ).data!,
           );
