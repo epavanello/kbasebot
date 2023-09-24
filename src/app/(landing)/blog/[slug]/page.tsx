704d35e4beada0acf09742b75ee69492cd07d2cb
nilooy <nilooy.dev@gmail.com>
2023-09-24 10:54:29 +0200

    :zap: Add content layer

---
+++ src/app/(landing)/blog/[slug]/page.tsx
@@ -0,0 +1,67 @@
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import Script from "next/script";
+
+import { allPosts } from "contentlayer/generated";
+import Post from "@/components/content/post";
+import Container from "@/components/ui/Container";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata | undefined> {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    return;
+  }
+
+  const { title, date, description, image, slug } = post;
+  const url = [process.env.NEXT_PUBLIC_URL, "blog", slug].join("/");
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      publishedTime: date,
+      url,
+      images: image
+        ? [
+            {
+              url: image,
+            },
+          ]
+        : [],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: image ? [image] : [],
+    },
+  };
+}
+
+async function Blog({ params }: { params: { slug: string } }) {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <Container>
+      <Script id={"ld-json"} type="application/ld+json">
+        {JSON.stringify(post.structuredData)}
+      </Script>
+
+      <Post post={post} content={post.body.code} />
+    </Container>
+  );
+}
+
+export default Blog;
---
+++ src/app/(landing)/blog/page.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { allPosts } from "contentlayer/generated";
+
+import PostPreview from "@/components/content/post-preview";
+import GridList from "@/components/content/post-preview";
+import Container from "@/components/ui/container";
+import SubHeading from "@/components/ui/sub-heading";
+import Heading from "@/components/ui/heading";
+
+export const metadata: Metadata = {
+  title: `Blog - KBaseBot`,
+  description: `Tutorials, Guides and Updates from our team`,
+};
+
+async function BlogPage() {
+  return (
+    <Container>
+      <div className={"flex flex-col space-y-16 my-8"}>
+        <div className={"flex flex-col items-center space-y-4"}>
+          <Heading type={1}>Blog</Heading>
+
+          <SubHeading>Tutorials, Guides and Updates from our team</SubHeading>
+        </div>
+
+        <GridList>
+          {allPosts.map((post, idx) => {
+            return <PostPreview key={idx} post={post} />;
+          })}
+        </GridList>
+      </div>
+    </Container>
+  );
+}
+
+export default BlogPage;
